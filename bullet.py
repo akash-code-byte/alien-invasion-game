e7e05c8b4959409472c349a073dbdd8b3ddca2cb
Akash <akashm.coder@gmail.com>
2025-02-21 11:52:45 +0530

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -1,1 +1,21 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = (255, 255, 255)
+
+        self.rect = pygame.Rect(0, 0, 3, 15)
+        self.rect.midtop = ai_game.ship.rect.midtop
+        self.y = float(self.rect.y)
+
+    def update(self):
+        self.y -= self.settings.bullet_speed
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
 
