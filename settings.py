90bdddd4e3391a15ddd2d0000a529268902f15e9
Akash <akashm.coder@gmail.com>
2025-02-21 11:53:04 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,11 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (30, 30, 30)
 
+        self.ship_speed = 5
+        self.bullet_speed = 7
+        self.bullets_allowed = 5
+
+        self.alien_speed = 2
